7b927d83cb8772c3ccd983e0b857e14221b37b2e
samir76 <abbasumar855@yahoo.com>
2025-02-22 23:13:20 +1100

    Update Data.js

--- src/database/Data.js
+++ src/database/Data.js
@@ -1,1 +1,107 @@
-
+export const data2 = [
+  {
+    common: "Yorkshire fog",
+    commonid: "442",
+    family: "Poaceae",
+    sciname: "Holcus lanatus",
+    desc: "Plant Form: Perennial tufted grass.\nSize: Up to 1 m tall. Stem: Round, green. Shaggy appearance due to covering of hairs. Leaves: Flat blades with smooth edges and pointed tips. Soft, covered in velvety hairs giving greyish appearance.\nFlowers: Dense elongated oval-shape and pinkish when young, opening up and becoming white with age.\nFruit and Seeds: 4-5 mm long, boat-shaped and hairy. Lower glumes (seed husks) are mostly awnless while upper florets have short hooked awns.\nHabitat: Grasslands, open woodlands, riparian areas, swamps. Distinguishing Features: Soft, hairy leaves and pink flowerheads distinguish from other grasses (Poaceae).",
+    status: "Non-priority Weed",
+    mytype: "Grassy",
+    imagesrc: [
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2258.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2259.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2260.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2261.jpg"
+    ],
+    claes: [
+      "Environmental"
+    ],
+    territoryprofiles: [],
+    territoryprofileshref: []
+  },
+  {
+    common: "Yellow-flowered Mexican poppy",
+    commonid: "219",
+    family: "Papaveraceae",
+    sciname: "Argemone mexicana",
+    desc: "Plant Form: Erect annual herb.\r\nSize: Up to 1.5 m tall.\r\nStem: Green, up to 1 m long and smooth or slightly covered in hairs, with yellow prickles.\r\nLeaves: Undivided, irregularly toothed to lobed, spiny, up to 12 cm long.\r\nFlowers: Generally bright yellow, up to 7 cm diameter.\r\nFruit and Seeds: Capsules up to 40 mm long, with many large, dark seeds.\r\nHabitat: Roadsides, degraded land.\r\nDistinguishing Features: Darker coloured flowers and fewer spines on capsules than Mexican Poppy (Argemone ochroleuca).",
+    status: "Priority Weed",
+    mytype: "",
+    imagesrc: [
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1011.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1012.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1013.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1014.jpg"
+    ],
+    claes: [
+      "Agricultural"
+    ],
+    territoryprofiles: [
+      "Weeds Australia",
+      "WAOL"
+    ],
+    territoryprofileshref: [
+      "https://weeds.org.au/profiles/mexican-poppy/",
+      "https://www.agric.wa.gov.au/declared-plants/mexican-poppy-weeds-western-australia"
+    ]
+  },
+  {
+    common: "Yellow oleander",
+    commonid: "23",
+    family: "Apocynaceae",
+    sciname: "Cascabela thevetia",
+    desc: "Plant Form: Large shrub or small tree.\r\nSize: Up to 4 m tall.\r\nStem: Wide spreading. Contain a milky sap.\r\nLeaves: Lance-shaped, 5-15 cm long, arranged alternately along stems. Glossy green upper surface, paler underneath.\r\nFlowers: Funnel-shaped yellow or peach-orange 5-7 cm long. With a strong sweet smell.\r\nFruit and Seeds: Oval, egg, or triangular-shaped, up to 3 cm diameter. Fleshy green when young, ripening to black, enclosing a stone with 2 kernels.\r\nHabitat: Forests, woodlands, riparian areas.",
+    status: "Priority Weed",
+    mytype: "Woody",
+    imagesrc: [
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2888.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2889.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2890.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/2891.jpg"
+    ],
+    claes: [
+      "Agricultural",
+      "Invasive Garden Plant"
+    ],
+    territoryprofiles: [
+      "Weeds Australia",
+      "DAF Qld"
+    ],
+    territoryprofileshref: [
+      "https://weeds.org.au/profiles/yellow-oleander-captain/",
+      "https://www.business.qld.gov.au/industries/farms-fishing-forestry/agriculture/biosecurity/plants/invasive/restricted/captain-cook-tree"
+    ]
+  },
+  {
+    common: "Yellow nutgrass",
+    commonid: "146",
+    family: "Cyperaceae",
+    sciname: "Cyperus esculentus",
+    desc: "Plant Form Slender perennial sedge. Size Up to 70 cm tall. Stem Triangular, smooth but increasingly rough towards top. Leaves Grass-like, glossy light green. V-shaped (in cross section) with finely serrated edges and a prominent mid-vein. Flowers 3-9 branching clusters of yellowish-brown spikelets. Fruit and Seeds Yellowish brown seeds which are triangular in cross-section. Habitat Irrigated farmland, disturbed areas, Distinguishing Features Very similar to Purple nutgrass (Cyperus rotundus) but has yellowish rather than purplish flowers.",
+    status: "Priority Weed",
+    mytype: "Grassy",
+    imagesrc: [
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1632.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1633.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1634.jpg",
+      "https://stweedscanprodaueast.blob.core.windows.net/examples/1635.jpg"
+    ],
+    claes: [
+      "Environmental",
+      "Agricultural"
+    ],
+    territoryprofiles: [
+      "Weeds Australia",
+      "NSW WeedWise",
+      "DNRE",
+      "NSW WeedWise"
+    ],
+    territoryprofileshref: [
+      "https://weeds.org.au/profiles/yellow-nutgrass-nut/",
+      "https://weeds.dpi.nsw.gov.au/Weeds/YellowNutgrass",
+      "https://nre.tas.gov.au/invasive-species/weeds/weeds-index/declared-weeds-index/yellow-nut-grass-yellow-nut-sedge",
+      "https://weeds.dpi.nsw.gov.au/Weeds/YellowNutgrass"
+    ]
+  }
+];
+ 
